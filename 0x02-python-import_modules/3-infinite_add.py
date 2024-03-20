5f643f6cd2fbdab128db8c6e67109a9a2d25e43b
BrendaLetseka1 <kgaitsediletseka@gmail.com>
2024-03-20 08:01:47 -0700

    import modules

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,20 @@
+
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    result = 0
+    for arg in sys.argv:
+        if arg != sys.argv[0]:
+            result += int(arg)
+    print(result)
+Latest commit
+77;30700;0cLatest commit
+77;30700;0c
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    result = 0
+    for arg in sys.argv:
+        if arg != sys.argv[0]:
+            result += int(arg)
+    print(result)
