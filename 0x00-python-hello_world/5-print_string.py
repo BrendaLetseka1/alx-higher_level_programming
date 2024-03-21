1cade3d3948b57fb2be1d7735f54e0b152aad4e2
BrendaLetseka1 <kgaitsediletseka@gmail.com>
2024-03-21 07:40:33 -0700

    hello world

---
+++ 0x00-python-hello_world/5-print_string.py
@@ -0,0 +1,4 @@
+
+#!/usr/bin/python3
+str = "software engineering"
+print(("{}\n{}").format(str*3, str[:9]))
---
+++ 0x00-python-hello_world/6-concat.py
@@ -0,0 +1,6 @@
+
+#!/usr/bin/python3
+str1 = "software"
+str2 = "Engineering"
+str1 = str1 + " " + str2
+print(f"Welcome to {str1}!")
